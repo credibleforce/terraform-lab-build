4e55569a0c10a47c6ffe37ceb5ee2e364b58e8f1
scalatar <jamiefrasermcmurray@gmail.com>
2021-02-27 13:59:15 -0800

    Update ansible base

--- templates/ansible_base.sh
+++ templates/ansible_base.sh
@@ -105,10 +105,12 @@ sudo git clone --branch develop --recursive https://github.com/mobia-security-se
 sudo git clone --branch develop --recursive https://github.com/mobia-security-services/splunk-lab /opt/repo/splunk-lab
 
 # sym link doesn't work (needs further test) just copy ansible directory - ideally structure of repo include ansible.cfg at the root for awx
-sudo mkdir -p /opt/awx/projects/splunk
-sudo mkdir -p /opt/awx/projects/lab
+sudo mkdir -p /opt/awx/projects
 sudo cp -pr /opt/repo/splunk-engagement-ansible/ansible /opt/awx/projects/splunk
 sudo cp -pr /opt/repo/splunk-lab/ansible /opt/awx/projects/lab
+
+# create an organization
+awx --conf.host=http://localhost:80 --conf.username=admin --conf.password=password --conf.insecure organization create --name "Default"
 
 # create an inventory place holder
 awx --conf.host=http://localhost:80 --conf.username=admin --conf.password=password --conf.insecure inventory create --name "lab-inventory" --organization "Default"
