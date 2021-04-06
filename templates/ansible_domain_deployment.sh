297e85d4e883152397d5411c52da929491c3786c
scalatar <jamiefrasermcmurray@gmail.com>
2021-04-05 19:51:40 -0700

    Update splunk

--- templates/ansible_domain_deployment.sh
+++ templates/ansible_domain_deployment.sh
@@ -18,4 +18,4 @@ awx --conf.host=http://localhost:80 --conf.username=admin --conf.password="${ans
 awx --conf.host=http://localhost:80 --conf.username=admin --conf.password="${ansible_awx_password}" --conf.insecure job_template associate --credential "lab-linux" --name "lab-template"
 
 # setup domain
-awx --conf.host=http://localhost:80 --conf.username=admin --conf.password="${ansible_awx_password}" --conf.insecure job_templates launch 'lab-template' --monitor -f human --extra-vars "@~/deployment/ansible/vars_base.yml"
+#awx --conf.host=http://localhost:80 --conf.username=admin --conf.password="${ansible_awx_password}" --conf.insecure job_templates launch 'lab-template' --monitor -f human --extra-vars "@~/deployment/ansible/vars_base.yml"
